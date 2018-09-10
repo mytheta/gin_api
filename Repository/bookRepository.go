d9dbfc12740c632582c40af864f1cef6b549d40a
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-10 15:57:56 +0900

    CRUD

--- Repository/bookRepository.go
+++ Repository/bookRepository.go
@@ -23,4 +23,15 @@ func (b *bookimpl) Update(book model.Book) error {
 	err := db.Save(&book).Error
 	return err
 }
+
+func (b *bookimpl) Delete(id uint) error {
+	book := model.Book{}
+	err :=  db.Delete(&book,id).Error
+	return err
+}
+
+func (b *bookimpl) IsExistByID(id uint) (bool,error){
+	var books []model.Book
+	err := db.Where("id = ?",id).Find(&books).Error
+	return len(books) >= 1,err
 }
